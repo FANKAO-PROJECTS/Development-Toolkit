e09b9c92ab4396e0b89f503881c0115edd4dcbb2
Chien Nguyen <36409877+fankao@users.noreply.github.com>
2024-08-02 17:59:20 +0700

    feat - update folder

--- java/framework/sample/Microservices-Spring-Cloud/rename.ps1
+++ java/framework/sample/Microservices-Spring-Cloud/rename.ps1
@@ -1,30 +1,20 @@
 # Define the mapping of chapter numbers to the transformed names
 $chapterMap = @{
-    "Chapter03"  = "3-cooperating-microservices"
-    "Chapter04"  = "4-deploy-docker"
-    "Chapter05"  = "5-api-description-openapi"
-    "Chapter06"  = "6-adding-persistence"
-    "Chapter07"  = "7-reactive-microservices"
-    "Chapter08"  = "8-intro-spring-cloud"
-    "Chapter09"  = "9-service-discovery-eureka"
-    "Chapter10" = "10-spring-cloud-gateway"
-    "Chapter11" = "11-securing-apis"
-    "Chapter12" = "12-central-config"
-    "Chapter13" = "13-resilience4j"
-    "Chapter14" = "14-distributed-tracing"
-    "Chapter15" = "15-intro-kubernetes"
-    "Chapter16" = "16-deploy-kubernetes"
-    "Chapter17" = "17-kubernetes-features"
-    "Chapter18" = "18-service-mesh"
-    "Chapter19" = "19-central-logging-efk"
-    "Chapter20" = "20-monitoring-microservices"
-    "Chapter21" = "21-install-macos"
-    "Chapter22" = "22-install-windows-ubuntu"
-    "Chapter23" = "23-native-java-microservices"
+    "chapter2"  = "2-creating-destroying-objects"
+    "chapter3"  = "3-common-methods"
+    "chapter4"  = "4-classes-interfaces"
+    "chapter5"  = "5-generics"
+    "chapter6"  = "6-enums-annotations"
+    "chapter7"  = "7-lambdas-streams"
+    "chapter8"  = "8-methods"
+    "chapter9"  = "9-general-programming"
+    "chapter10" = "10-exceptions"
+    "chapter11" = "11-concurrency"
+    "chapter12" = "12-serialization"
 }
 
 # Path to the main folder containing the chapter folders
-$mainFolderPath = "D:\Working\1.PROJECT\Development-Toolkit\java\framework\sample\Microservices-Spring-Cloud"
+$mainFolderPath = "D:\Working\1.PROJECT\Development-Toolkit\java\core\effective-java-3e\src\effectivejava"
 
 # Loop through each mapping and rename the corresponding folders
 foreach ($chapter in $chapterMap.Keys) {
